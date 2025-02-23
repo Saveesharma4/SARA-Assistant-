ff80d0e019f33fea0a3bf0db67cc10daf521ca9b
Savee Sharma <145105565+Saveesharma4@users.noreply.github.com>
2025-02-23 19:17:09 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -2,5 +2,5 @@ music = {
      "hell": " https://youtu.be/AtnB0DbuFFU?si=5In1FQQuCzet87im",
      "stupid":"https://youtu.be/T94PHkuydcw?si=zSEmT5eRndQCXt5T",
       "feel":"https://youtu.be/jmpUP1MaQ9Q?si=0YUYoq5g6HJolUcz"
-
+#add your fav songs
 }
